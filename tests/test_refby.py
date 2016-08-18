2830b9a8f8ad8849a47eea30f2f949f3515363b5
Santos Gallegos <santos_g@outlook.com>
2016-08-18 13:24:10 -0500

    'Spanglish'

--- tests/test_refby.py
+++ tests/test_refby.py
@@ -7,8 +7,8 @@ class TestRefby(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_usuario_publico_krysthyan09(self):
+    def test_public_user_krysthyan09(self):
         self.assertEqual(refby.get_user_id('krysthyan09'), '100009719676455')
 
-    def test_usuario_publico_pedropolisdo(self):
+    def test_public_user_pedropolisdo(self):
         self.assertEqual(refby.get_user_id('pedropolisdo'), '1202200477')
